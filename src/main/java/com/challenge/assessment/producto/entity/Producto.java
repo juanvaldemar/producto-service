f23415e924d3a5db3d6a7c7c216b2b959ac866e2
DonValdemar <juan.colichon@vasslatam.com>
2021-05-25 18:51:54 -0500

    Agregando variables

--- src/main/java/com/challenge/assessment/producto/entity/Producto.java
+++ src/main/java/com/challenge/assessment/producto/entity/Producto.java
@@ -24,6 +24,9 @@ public class Producto {
     @Column(name= "tecnologia")
     private String tecnologia;
 
+    @Column(name= "numero_documento")
+    private String numero_documento;
+
     @Column(name= "fecha_grabacion")
     private Date fecha_grabacion = new Date();
 }
